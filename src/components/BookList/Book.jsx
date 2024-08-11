0074b07b5e7bb86ec771fa6e8058e6ff7e2785b4
MATE0014 <mate022814@gmail.com>
2024-08-11 21:29:11 +0530

    v1.0.2

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -11,33 +11,33 @@ const Book = ({
   first_publish_year,
 }) => {
   return (
-    <div className="book-item flex items-center flex-col justify-between">
-      <div className="book-item-img">
-        <img src={cover_img} alt="cover" />
-      </div>
-      <div className="book-item-info text-center">
-        <Link to={`/book/${id}`}>
+    <Link to={`/book/${id}`}>
+      <div className="book-item flex items-center flex-col justify-between">
+        <div className="book-item-img">
+          <img src={cover_img} alt="cover" />
+        </div>
+        <div className="book-item-info text-center">
           <div className="book-item-info-item title font-bold text-[1.8rem] text-orange-800">
             <span>{title}</span>
           </div>
-        </Link>
 
-        <div className="book-item-info-item author text-[1.5rem]">
-          <span className="capitalize font-bold">Author: </span>
-          <span>{author.join(", ")}</span>
-        </div>
+          <div className="book-item-info-item author text-[1.5rem]">
+            <span className="capitalize font-bold">Author: </span>
+            <span>{author.join(", ")}</span>
+          </div>
 
-        <div className="book-item-info-item edition-count text-[1.5rem]">
-          <span className="capitalize font-bold">Total Editions: </span>
-          <span>{edition_count}</span>
-        </div>
+          <div className="book-item-info-item edition-count text-[1.5rem]">
+            <span className="capitalize font-bold">Total Editions: </span>
+            <span>{edition_count}</span>
+          </div>
 
-        <div className="book-item-info-item publish-year text-[1.5rem]">
-          <span className="capitalize font-bold">First Published In: </span>
-          <span>{first_publish_year}</span>
+          <div className="book-item-info-item publish-year text-[1.5rem]">
+            <span className="capitalize font-bold">First Published In: </span>
+            <span>{first_publish_year}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
