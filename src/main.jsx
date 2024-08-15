e7a9d75d58b0b981cb10466284b12441b9062a21
MATE0014 <mate022814@gmail.com>
2024-08-15 18:28:48 +0530

    Stability Upgrade

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./index";
 import "./index.css";
 import Home from "./pages/Home/Home";
@@ -12,16 +12,11 @@ createRoot(document.getElementById("root")).render(
   <AppProvider>
     <BrowserRouter>
       <Routes>
-        {/* Home Route */}
         <Route path="/" element={<Home />}>
-          {/* Nested Routes */}
           <Route path="about" element={<About />} />
           <Route path="book" element={<BookList />} />
-          <Route path="book/:id" element={<BookDetails />} />
+          <Route path="/book/:id" element={<BookDetails />} />
         </Route>
-
-        {/* Catch-all route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </AppProvider>
